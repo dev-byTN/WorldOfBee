a1a58cc74d10417132227034843948e7cf401fbe
nazibinks <81704481+nazibinks@users.noreply.github.com>
2023-12-17 13:54:36 +0100

    worldOfBee.h

--- worldOfBee.h
+++ worldOfBee.h
@@ -1,9 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include <stdbool.h>
 #include <string.h>
+#include <SDL2/SDL.h>
 
 #define SizeOfBeehive 10
+#define NB_QUEEN 1
+#define NB_WORKER 3
+#define NB_GUARDIAN 2
+#define NB_DRONE 2
+#define NB_FORAGER 2
 
 
 typedef struct Bee Bee;
@@ -12,17 +19,20 @@ typedef struct Queen Queen;
 typedef struct Drone Drone;
 typedef struct Guardian Guardian;
 typedef struct Worker Worker;
-typedef struct Seasons Seasons;
+typedef struct Forager Forager;
 
 struct Bee
 {
+    int iLifeTime;
     int iIdOfBee;
-    int IYearOfBee;
-    char cTypeOfBee[10];
+    int iNbQueens;
+    
     Bee *next;
     Queen *queen;
     Guardian *guardian;
     Drone *drone;
+    Worker *worker;
+    Forager *forager;
 
 };
 
@@ -35,24 +45,50 @@ struct ListOfBee
 struct Queen
 {
     int iLifeTime;
-
+    int iIdOfBee;
+    int iNbQueens;
+    Bee *next;
 };
 
 struct Guardian
 {
     int iLifeTime;
-
+    int iIdOfBee;
+    int iNbQueens;
+    Bee *next;
 };
 
 struct Drone
 {
     int iLifeTime;
+    int iIdOfBee;
+    int iNbQueens;
+    Bee *next;
 
 };
 
+struct Worker
+{
+    int iLifeTime;
+    int iIdOfBee;
+    int iNbQueens;
+    Bee *next;
+};
+
+struct Forager
+{
+    int iLifeTime;
+    int iIdOfBee;
+    int iNbQueens;
+    Bee *next;
+};
 
 ListOfBee *InitialisationListBee();
 Bee *BirthOfBee ( ListOfBee *myList);
 Bee *deathOfBee ( ListOfBee *myList, int idOfBee);
 void displayBeehive ( ListOfBee *myList);
 int creationOfBeehive ( ListOfBee *myList );
+int deathOfTheQueen ( ListOfBee *myList, int yearOfBee);
+int deathOfDrone ( ListOfBee *myList);
+int deathOfWorker ( ListOfBee *myList);
+int varroaDisability(ListOfBee *myList);
