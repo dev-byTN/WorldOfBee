c12e9d7a8f1d33e1cbe786e11f8889926d26a181
nazibinks <81704481+nazibinks@users.noreply.github.com>
2023-12-17 13:53:53 +0100

    worldOfBee.c

--- worldOfBee.c
+++ worldOfBee.c
@@ -12,27 +12,30 @@ ListOfBee *InitialisationListBee()
 
     
     myList->first = NULL;
-    myList->iNbOfBee ++;
+    myList->iNbOfBee =0;
     return myList;
 }
 
 int creationOfBeehive ( ListOfBee *myList ) // ceration de la ruche avec 10 abeilles
 {
+    int i;
     
-
     if ( myList == NULL)
     {
         exit(EXIT_FAILURE);
     }
 
-    for ( int i = 0; i < SizeOfBeehive; i ++)
+    for ( i = 0; i < SizeOfBeehive; i ++) 
     {
         Bee *creationOfBee = malloc(sizeof(*creationOfBee));
         creationOfBee->iIdOfBee = SizeOfBeehive - i;
+        creationOfBee->iLifeTime = 0;
         creationOfBee->next = myList->first;
         myList->first = creationOfBee;
         myList->iNbOfBee ++;
     }
+
+   
 }
 
 
@@ -42,7 +45,7 @@ Bee *BirthofBee(ListOfBee *myList) // chaque abeilles qui naissent apparaitront 
     Bee *newBee = malloc(sizeof(*newBee));
     Bee *temp = myList->first;
 
-    newBee->iIdOfBee = myList->iNbOfBee;
+    //newBee->iIdOfBee = myList->iNbOfBee;
     newBee->next = NULL;
 
     if (myList == NULL )
@@ -74,7 +77,7 @@ Bee *deathOfBee ( ListOfBee *myList, int idOfBee)
 
     while (temp->next != NULL )
     {
-        if ( temp->iIdOfBee == idOfBee )
+        //if ( temp->iIdOfBee == idOfBee )
         {
             previoustemp->next = temp->next;
             free(temp);
@@ -82,7 +85,7 @@ Bee *deathOfBee ( ListOfBee *myList, int idOfBee)
         previoustemp = temp;
         temp = temp->next;
     }
-    BirthofBee(myList);
+    return BirthofBee(myList);
     
 }
 
@@ -98,7 +101,7 @@ int deathOfTheQueen ( ListOfBee *myList, int yearOfBee)
 
     while ( queen != NULL)
     {
-        if ( queen->IYearOfBee == yearOfBee )
+        //if ( queen->IYearOfBee == yearOfBee )
         {
             
         }
@@ -116,10 +119,123 @@ void displayBeehive ( ListOfBee *myList)
 
     while ( temp != NULL )
     {
-        printf("%d", temp->iIdOfBee);
+        printf("%d ", temp->iIdOfBee);
         //printf("%d", temp->queen);
         temp = temp->next;
         printf(" -> ");
     }
     printf("NULL\n");
+}
+
+
+int deathOfDrone ( ListOfBee *myList)
+{
+    Bee *deathBee = myList->first;
+    if ( myList == NULL )
+    {
+        exit (EXIT_FAILURE);
+    }
+
+    while ( deathBee != NULL)
+    {
+        if ( deathBee->drone->iLifeTime == 14 )
+        {
+           // return deathOfBee(myList, deathBee->drone->iIdOfBee);
+        }
+        else deathBee = deathBee->next;
+    }
+}
+
+int deathOfWorker ( ListOfBee *myList)
+{
+    Bee *deathBee = myList->first;
+    if ( myList == NULL )
+    {
+        exit (EXIT_FAILURE);
+    }
+
+    while ( deathBee != NULL)
+    {
+        if ( deathBee->worker->iLifeTime == 20 )
+        {
+            //deathBee->worker = deathBee->forager;
+        }
+        else deathBee = deathBee->next;
+    }
+}
+
+
+int createQueens( ListOfBee *myList, int numQueens) 
+{
+
+    if (myList == NULL) {
+        exit (EXIT_FAILURE);
+    }
+
+    for (int i = 0; i < numQueens; i++) 
+    {
+        Bee *creationOfBee = malloc(sizeof(*creationOfBee));
+
+        if (creationOfBee == NULL) 
+        {
+            exit (EXIT_FAILURE);
+        }
+
+        creationOfBee->queen = malloc(sizeof(Queen));
+
+        if (creationOfBee->queen == NULL) {
+            free(creationOfBee); // Libération de la mémoire si l'allocation échoue
+            return -1; // Gestion d'erreur : échec de l'allocation mémoire
+        }
+
+        creationOfBee->queen->iLifeTime = 0;
+        creationOfBee->queen->iIdOfBee = i;
+        creationOfBee->queen->next = myList->first;
+
+        /*Initialisation des autres pointeurs à NULL
+        creationOfBee->next = NULL;
+        creationOfBee->guardian = NULL;
+        creationOfBee->drone = NULL;
+        creationOfBee->worker = NULL;
+        creationOfBee->forager = NULL;*/
+
+        myList->first = creationOfBee;
+        myList->iNbOfBee++;
+    }
+
+    return 0; 
+}
+
+int createDrone( ListOfBee *myList, int numDrone) 
+{
+
+    if (myList == NULL) {
+        exit (EXIT_FAILURE);
+    }
+
+    for (int i = 0; i < numDrone; i++) 
+    {
+        Bee *creationOfBee = malloc(sizeof(*creationOfBee));
+
+        if (creationOfBee == NULL) 
+        {
+            exit (EXIT_FAILURE);
+        }
+
+        creationOfBee->drone = malloc(sizeof(Drone));
+
+        if (creationOfBee->drone == NULL) {
+            free(creationOfBee); 
+            exit(EXIT_FAILURE); 
+        }
+
+        creationOfBee->drone->iLifeTime = 0;
+        creationOfBee->drone->iIdOfBee = i;
+        creationOfBee->drone->next = myList->first;
+
+        myList->first = creationOfBee;
+        myList->iNbOfBee++;
+    }
+
+    return 0; 
 }
